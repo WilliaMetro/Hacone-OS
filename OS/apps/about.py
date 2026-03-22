bfa600e6c40981cb3b14fd3b90256084a8e71a5f
WilliaMetro <williametronext@gmail.com>
2026-03-22 20:51:31 +0700

    yup

--- OS/apps/about.py
+++
@@ -1,5 +0,0 @@
-def run():
-    print("\nFakeOS v1.0")
-    print("Made by William 😎")
-
-    input("Press Enter to return...")
--- OS/system/loader.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-def load_app(app_name):
-    try:
-        module = importlib.import_module(f"apps.{app_name}")
-        module.run()
-    except Exception as e:
-        print(f"Error loading app: {e}")
